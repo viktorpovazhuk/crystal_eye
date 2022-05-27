435ad1f96ee4026e7e9faa1f33fcceb05cb1fe6e
PHentosh <71725222+PHentosh@users.noreply.github.com>
2022-05-27 22:01:39 +0300

    Update patchExtractor.py

--- patchExtractor.py
+++ patchExtractor.py
@@ -3,6 +3,17 @@
 class patch_extractor:
     extraction_region = ((),())
 
+    def is_overlap(img,  target_region = extraction_region):
+        # If one rectangle is on left side of other
+        if(img[0][0] > target_region[1][0] or target_region[0][0] > img[1][0]):
+            return False
+    
+        # If one rectangle is above other
+        if(img[1][1] < target_region[0][1] or target_region[1][1] < img[0][1]):
+            return False
+    
+        return True    
+
     def setDesieredRegion(self, extraction_region):
         self.extraction_region = extraction_region
 
@@ -36,5 +47,29 @@ class patch_extractor:
                     patches.append(patch)
         
         return patches
+
+    def get_patches_by_size(self, img, x_scale, y_scale, size, target_region = extraction_region):
+        
+        final_list = []
+
+        y_max, x_max = img.shape[0], img.shape[1]
+
+        for i in range(0, y_max - size, y_scale):
+            for j in range(0, x_max - size, x_scale):
+                if not self.is_overlap(((j, i), (j+size, i+size)), target_region):
+                    final_list.append(img[i:i+size, j:j+size])
+                if j+size+x_scale >= y_max:
+                    if not self.is_overlap(((x_max-size, i), (x_max, i+size)), target_region):
+                        final_list.append(img[i:i+size, x_max-size:x_max])
+            if i+size+y_scale >= y_max:
+                i = y_max - size
+                for j in range(0, x_max - size, x_scale):
+                    if not self.is_overlap(((j, i), (j+size, i+size)), target_region):
+                        final_list.append(img[i:i+size, j:j+size])
+                    if j+size+x_scale >= y_max:
+                        if not self.is_overlap(((x_max-size, i), (x_max, i+size)), target_region):
+                            final_list.append(img[i:i+size, x_max-size:x_max])
+        
+        return final_list
 
 

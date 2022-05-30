46d3b6fc9b325c7cb6edb0cf66c244a7dc2a5c05
PHentosh <71725222+PHentosh@users.noreply.github.com>
2022-05-30 18:45:47 +0300

    Add files via upload

---
+++ src/start.py
@@ -0,0 +1,33 @@
+from main import main
+
+f = open("config.txt", "r")
+
+properties = []
+pict = {}
+
+for i in f.readlines():
+    if i == "...\n":
+        properties.append(pict)
+        pict = {}
+    else:
+        key, val = i.split("=")[0], i.split("=")[1]
+        key = key.strip()
+        val = val.strip()
+        
+        if key == "path":
+            pict[key] = val
+        
+        elif key == "target_region":
+            coords = val.split(", ")
+            pict[key] = (int(coords[0].strip()), int(coords[1].strip()), int(coords[2].strip()), int(coords[3].strip()))
+
+        elif key == "beta":
+            pict[key] = float(val)
+        else:
+            pict[key] = int(val)
+
+for i in properties:
+    main(i["path"], i["target_region"], i["beta"], i["x_step"] ,i["y_step"], i["patch_size"])
+    
+    
+
